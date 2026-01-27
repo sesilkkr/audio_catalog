f7459638fc43e6a8ca16d2df7b9501cbce59f9e3
sesil <sesil.kkr@gmail.com>
2026-01-27 23:59:14 +0200

    Add AudioItem model

--- src/model/AudioItem.java
+++ src/model/AudioItem.java
@@ -21,25 +21,43 @@ public AudioItem(String title, String genre, int duration, String author, int ye
     public String getTitle() {
         return title;
     }
+    public void setTitle(String title) {
+       this.title = title;
+    }
 
     public String getGenre() {
         return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
     public int getDuration() {
         return duration;
     }
+    public void setDuration(int duration) {
+       this.duration = duration;
+    }
 
     public String getAuthor() {
         return author;
+    }
+    public void setAuthor(String author) {
+    this.author = author;
     }
 
     public int getYear() {
         return year;
     }
+    public void setYear(int year) {
+    this.year = year;
+    }
 
     public String getCategory() {
         return category;
+    }
+    public void setCategory(String category) {
+    this.category = category;
     }
 
     @Override
